992cd7c663fc03ce1bede963c68ab624f92b3883
AlumnoMañana <AlumnoMaana@T20211>
2019-02-07 10:06:34 +0000

    07/02/2019

--- src/control/Ejercicios.java
+++ src/control/Ejercicios.java
@@ -37,9 +37,7 @@ import modelo.Persona;
  *
  */
 public class Ejercicios {
-	
 
-	
 	
 	
 // ---------------  04/10/2018                      ACTIVIDAD: BUSCAR EN LA API -------------------------------
